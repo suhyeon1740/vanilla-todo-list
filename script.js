b0d9cc9dd8d445605053deb595b3dcfd242a7fd6
elly <suhyeon1740@gmail.com>
2019-10-08 10:28:22 +0900

    css적용

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
 const makeList = (content) => {
     let li = document.createElement('li')
     li.innerHTML = `<label><input type="checkbox">${content}</label>`
-    let btn = document.createElement('button')
-    btn.innerText = '삭제'
+    let btn = document.createElement('div')
+    btn.classList.add('trash')
+    btn.innerHTML = '<i class="fas fa-trash-alt"></i>'
     btn.addEventListener('click', removeList)
     li.appendChild(btn)
     li.addEventListener('click', clickList)
     document.getElementById('list').appendChild(li)
 }
 
-const removeList = (e) => {    
-    document.getElementById('list').removeChild(e.target.parentNode)    
+const removeList = (e) => {       
+    if (e.target.tagName === 'DIV') {
+        document.getElementById('list').removeChild(e.target.parentNode)    
+    } else {
+        document.getElementById('list').removeChild(e.target.parentNode.parentNode)    
+    }
 }
 
 const clickList = (e) => {
     // TODO: li를 클릭해도 똑같이 동작해야 함
     if (e.target.tagName === 'INPUT') {
-        if (!e.target.checked)
-            e.target.parentNode.style.textDecoration = 'none'
-        else e.target.parentNode.style.textDecoration = 'line-through'
+        if (e.target.checked)
+            e.target.parentNode.classList.add('checked')
+            //e.target.parentNode.style.textDecoration = 'none'
+        else e.target.parentNode.classList.remove('checked')
     }
 }
 

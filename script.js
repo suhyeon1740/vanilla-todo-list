ee6cf5b3edadbdc6f2943a08f45725f461f3032f
elly <suhyeon1740@gmail.com>
2020-04-19 10:47:57 +0900

    post, patch, delete ajax 추가

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const makeList = (todo) => {    
     document.getElementById('list').insertAdjacentHTML('beforeend',`
-        <li>
+        <li id=${todo.id}>
             <input type="checkbox"><label>${todo.title}</label>
             <div class="trash">
                 <i class="fas fa-trash-alt"></i>
@@ -15,19 +15,37 @@ const makeList = (todo) => {
 }
 
 const removeList = (e) => {
-    var target = e.target.tagName == 'I' ? e.target.parentNode : e.target            
-    document.getElementById('list').removeChild(target.parentNode)
+    var target = e.target.tagName == 'I' ? e.target.parentNode : e.target
+    var id = target.parentElement.id
+    fetch(`http://localhost:3000/todos?id=${id}`, {
+        method: 'DELETE'
+    })
+    .then(() => document.getElementById('list').removeChild(target.parentNode))      
 }
 
 const clickList = (e) => {        
     var target  = e.target
+    var id = e.target.parentElement.id
     if(e.target.tagName == 'LABEL') {
         e.target.previousElementSibling.checked = !e.target.previousElementSibling.checked
         target = e.target.previousElementSibling        
     } 
-    if (target.checked)
-        target.parentNode.classList.add('checked')
-    else target.parentNode.classList.remove('checked')        
+    fetch('http://localhost:3000/todos', {
+        method: 'PATCH',
+        body: JSON.stringify({
+            id: id,
+            completed: target.checked
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(() => {
+        // if (target.checked)
+        //     target.parentNode.classList.add('checked')
+        // else target.parentNode.classList.remove('checked')
+        getData()
+    })
 }
 
 const addList = () => {
@@ -36,9 +54,20 @@ const addList = () => {
         alert('내용을 입력하세요.')
         return
     }
-    makeList({
-        title: content,
-        completed: false
+    let data = {
+        title: content
+    }
+    fetch('http://localhost:3000/todos', {
+        method: 'POST',
+        body: JSON.stringify(data), // JavaScript 값이나 객체를 JSON 문자열로 변환합니다
+        headers: {
+            // HTTP 요청을 하게 되면 서버가 JSON 타입으로 변환해서 사용한다.
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res)
+    .then(() => {
+        getData()
+        // makeList()
     })
     document.getElementById('content').value = ''
 }
@@ -61,7 +90,7 @@ document.getElementById('content').addEventListener('keydown', e => {
 
 getData()
 function getData() {
-    fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
+    fetch('http://localhost:3000/todos')
     .then(res => {
         if (res.status == 200) {
             return res.json()
@@ -70,6 +99,7 @@ function getData() {
         }
     })
     .then(jsonData => {
+        document.getElementById('list').innerHTML = ""
         for (const index in jsonData) {
             makeList(jsonData[index])
         }
